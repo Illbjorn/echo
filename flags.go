fda009a7f73c56bdbb5fe63e7a2e7bb0fb2bee2d
Illbjorn <am@hades.so>
2025-01-13 08:57:36 -0500

    Add Log Prefix Support

--- flags.go
+++ flags.go
@@ -6,7 +6,14 @@ import (
 	"strconv"
 )
 
-var flags Flag
+var (
+	flags  Flag
+	prefix string
+)
+
+func SetPrefix(p string) {
+	prefix = p
+}
 
 func SetFlags(f Flag) {
 	flags = f
@@ -17,9 +24,16 @@ type Flag uint8
 const (
 	FlagWithLevel Flag = 1 << iota
 	FlagWithCaller
+	FlagWithPrefix
 )
 
 func (f Flag) Write(l Level) {
+	if flags&FlagWithPrefix == FlagWithPrefix {
+		// Produce prefix
+		write([]byte(prefix))
+		write(space)
+	}
+
 	if flags&FlagWithLevel == FlagWithLevel {
 		// Produce level
 		write(l.Bytes())
